a4d7a2503e78d37ced5aefc8c34a6bff12faffde
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-10-18 11:11:55 +0800

    Create DeleteListingNotAllowedException Create InvalidListingSearchException

---
+++ src/main/java/com/test/staybooking/listing/DeleteListingNotAllowedException.java
@@ -0,0 +1,10 @@
+package com.test.staybooking.listing;
+
+
+public class DeleteListingNotAllowedException extends RuntimeException {
+
+
+   public DeleteListingNotAllowedException(String message) {
+       super(message);
+   }
+}
