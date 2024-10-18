e754175af1e46d0272a42883fee6762ef33fd183
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-10-18 10:56:46 +0800

    Create ImageStroageService

---
+++ src/main/java/com/test/staybooking/storage/ImageStorageService.java
@@ -0,0 +1,16 @@
+package com.test.staybooking.storage;
+
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Service
+public class ImageStorageService {
+
+
+   public String upload(MultipartFile file) {
+       // TODO: upload image to GCS
+       return "https://storage.googleapis.com/staybooking/placeholder.jpeg";
+   }
+}
