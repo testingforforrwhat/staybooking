3649c97f9ad7928cd4deef8af366fddbef5de27d
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-10-20 01:16:59 +0800

    Import storage bucket credential Update application.yml Update Appconfig

--- src/main/java/com/test/staybooking/AppConfig.java
+++ src/main/java/com/test/staybooking/AppConfig.java
@@ -1,6 +1,10 @@
 package com.test.staybooking;
 
 
+import com.google.auth.Credentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,21 +12,31 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 
+import java.io.IOException;
+
+
 @Configuration
 public class AppConfig {
 
 
-   @Bean
-   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-       http
-               .csrf().disable()
-               .authorizeHttpRequests(auth ->
-                       auth
-                               .requestMatchers( "/**").permitAll()
-                               .anyRequest().authenticated()
-               )
-               .sessionManagement()
-               .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-       return http.build();
-   }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers( "/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
+    }
+
+
+    @Bean
+    public Storage storage() throws IOException {
+        Credentials credentials = ServiceAccountCredentials.fromStream(getClass().getClassLoader().getResourceAsStream("credentials.json"));
+        return StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+    }
 }
