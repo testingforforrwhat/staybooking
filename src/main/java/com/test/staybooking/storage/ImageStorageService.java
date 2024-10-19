f1225db7483d4e0cd8c62e28b221daf66ca1cd2c
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-10-20 01:26:43 +0800

    Import storage bucket credential Update application.yml Update Appconfig update ImageStorageService.java

--- src/main/java/com/test/staybooking/storage/ImageStorageService.java
+++ src/main/java/com/test/staybooking/storage/ImageStorageService.java
@@ -1,16 +1,48 @@
+
 package com.test.staybooking.storage;
 
 
+import com.google.cloud.storage.Acl;
+import com.google.cloud.storage.BlobInfo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import com.google.cloud.storage.Storage;
+
+
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
 
 
 @Service
 public class ImageStorageService {
 
 
-   public String upload(MultipartFile file) {
-       // TODO: upload image to GCS
-       return "https://storage.googleapis.com/staybooking/placeholder.jpeg";
-   }
+    private final String bucketName;
+    private final Storage storage;
+
+
+    public ImageStorageService(@Value("${staybooking.gcs.bucket}") String bucketName, Storage storage) {
+        this.bucketName = bucketName;
+        this.storage = storage;
+    }
+
+
+    public String upload(MultipartFile file) {
+        String filename = UUID.randomUUID().toString();
+        BlobInfo blobInfo;
+        try {
+            blobInfo = storage.createFrom(
+                    BlobInfo
+                            .newBuilder(bucketName, filename)
+                            .setContentType("image/jpeg")
+                            .setAcl(List.of(Acl.of(Acl.User.ofAllUsers(), Acl.Role.READER)))
+                            .build(),
+                    file.getInputStream());
+        } catch (IOException exception) {
+            throw new IllegalStateException("Failed to upload file to GCS");
+        }
+        return blobInfo.getMediaLink();
+    }
 }
